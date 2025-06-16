603aef830eae36a2d2473f21bbfa7a10f2376b61
yankdownunderbbq <yankdownunderbbq@gmail.com>
2025-06-16 22:49:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Stripe = require('stripe');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 app.use(cors());
