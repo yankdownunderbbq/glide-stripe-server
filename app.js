2c0e7220e96b7beef544ea024a7685829d47d522
yankdownunderbbq <yankdownunderbbq@gmail.com>
2025-07-04 11:47:23 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const validateEnv = require('./utils/validateEnv');
+require('dotenv').config();
 
 validateEnv(); // Ensure env vars are present
 
@@ -10,7 +10,6 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
 app.use(require('./middleware/logger'));
 
 // Static file serving
@@ -21,6 +20,8 @@ app.use('/', require('./routes/index'));
 app.use('/stripe', require('./routes/stripe'));
 app.use('/terminal', require('./routes/terminal'));
 app.use('/', require('./routes/webhooks')); // Webhooks like /webhook-quotes etc.
+
+app.use(express.json());
 
 // Error Handling
 app.use(require('./middleware/errorHandler'));
